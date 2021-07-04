71d995fb5701ef3667805d721a8d2288b6f03e33
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2021-07-03 21:46:48 -0400

    Initial Commit

---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Ryan Spishock on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
